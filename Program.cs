8bf709bcb4bd500dbb5c946f3b5d3471d33e6e65
agustinpaolucci <globalprint@gmail.com>
2022-11-27 19:16:24 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PI_P1_1_Rectangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
+
+/*
+ Problema 1.1:
+Rectángulo. Calcular el perímetro y la superficie de un rectángulo de base 3 y alto 5.
+ */
