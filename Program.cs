544aa5245dd32f76c33db98254f4216c4f03ae75
agustinpaolucci <globalprint@gmail.com>
2022-11-27 20:23:56 -0300

    Final del Programa
    Creacion de objeto, ingreso de datos por consola y resultado.

--- Program.cs
+++ Program.cs
@@ -10,8 +10,19 @@ namespace PI_P1_1_Rectangulo
     {
         static void Main(string[] args)
         {
-            
-            
+            // CREACION DEL OBJETO
+            Rectangulo rec01 = new Rectangulo();
+
+            // INGRESO DE DATOS
+            Console.WriteLine("Ingrese la base del rectangulo en cms.");
+            rec01.BaseR = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la altura del rectangulo en cms.");
+            rec01.AlturaR = double.Parse(Console.ReadLine());
+
+            // CALCULOS
+            Console.WriteLine($"Teniendo un cuadrado rectangulo de BASE: {rec01.BaseR} cms. y ALTURA: {rec01.AlturaR} cms...\n\nSu PERIMETRO ES: {rec01.CalcularPerimetro()} cms. y su SUPERFICIE: {rec01.CalcularSuperficie()} cm3s.\n");
+            Console.WriteLine("Presione una tecla para terminar...");
+            Console.ReadKey();
         }
     }
 }
